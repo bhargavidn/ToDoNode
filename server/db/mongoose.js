cb696a95f483b4501690c7d9328a2a60d3389b6e
bhargavi.dn <bhargavi.bh92@gmail.com>
2018-04-11 20:50:40 +0530

    changed connect options

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose=require("mongoose");
 
 //connect to mongodb
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp",{
+  useMongoClient: true
+});
 
 //mongoose.connect("mongodb://bhargavi.dn:Bhagi@123@ds239009.mlab.com:39009/todolist");
 //to use promise
