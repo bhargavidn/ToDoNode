664ec5338e8cd871c697440d35e8d00679175e52
bhargavi.dn <bhargavi.bh92@gmail.com>
2018-04-12 20:26:34 +0530

    added remove route

--- server/server.js
+++ server/server.js
@@ -43,6 +43,22 @@ app.get("/todos/:id",(req,res)=>{
   }).catch((e)=>res.status(400).send({}))
 
 });
+//delete
+
+app.delete("/todos/:id",(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  toDo.findByIdAndRemove(id).then((result)=>{
+    console.log("result from remove ",result);
+    if(!result){
+      return res.status(404).send();
+    }
+    res.send({result})
+  }).catch((e)=>res.status(400).send({}))
+});
+
 app.listen(port,()=>{
   console.log(`Server started on ${port}`);
 });
